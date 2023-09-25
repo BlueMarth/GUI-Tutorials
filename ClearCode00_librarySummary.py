4a947e3a9a5de25dca7c8774c2d461366c418af5
BlueMarth <hubert.lcr@gmail.com>
2023-09-25 12:41:47 +0800

    Create ClearCode00_librarySummary.py

---
+++ ClearCode00_librarySummary.py
@@ -0,0 +1,25 @@
+# imports
+import tkinter as tk
+from tkinter import ttk
+
+
+# setups
+rootWidth = 800
+rootHeight = 500
+rootOffsetx = 0
+rootOffsety = 0
+root_geometry = str(rootWidth) + 'x' + str(rootHeight) + '+' + str(rootOffsetx) + '+' + str(rootOffsety)
+print(root_geometry)
+root = tk.Tk()
+root.geometry(root_geometry)
+root.title('Root')
+
+
+# widgets
+
+
+
+
+
+# run
+root.mainloop()
